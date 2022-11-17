6a35c5a2e10d6894a0c427bd4b66e8d32728d28f
Thorsten Maier <thorsten.a.maier@accenture.com>
2022-11-17 14:16:40 +0100

    CombineLatest Mail Address

--- reactive-app/src/main/java/clickortime/ClickOrTime.java
+++ reactive-app/src/main/java/clickortime/ClickOrTime.java
@@ -6,16 +6,21 @@ import java.util.concurrent.TimeUnit;
 
 public class ClickOrTime {
     public static void main(String[] args) throws InterruptedException {
+
+        Observable<String> systemEmail = Observable.just("admin@trivadis.com");
+
         Observable<TimeEvent> timer = Observable.interval(3, TimeUnit.SECONDS)
                 .map(i -> new TimeEvent());
 
         Observable<ClickEvent> clicks = Observable.interval(5, TimeUnit.SECONDS)
                 .map(i -> new ClickEvent());
 
-        Observable<MailEvent> mailEventsAusTimer = timer.map(t -> new MailEvent());
-        Observable<MailEvent> mailEventsAusClicks = clicks.map(t -> new MailEvent());
+        Observable<MailEvent> mailEventsAusTimer = timer.map(t -> new MailEvent("Timer"));
+        Observable<MailEvent> mailEventsAusClicks = clicks.map(t -> new MailEvent("Click"));
+        Observable<MailEvent> allMailEvents = mailEventsAusTimer.mergeWith(mailEventsAusClicks);
 
-        mailEventsAusTimer.mergeWith(mailEventsAusClicks)
+        Observable.combineLatest(allMailEvents, systemEmail,
+                        (mailEvent, mail) -> new MailEvent(mailEvent.getMessage(), mail))
                 .blockingSubscribe(e -> {
                     System.out.println("SEND MAIL " + e);
                 });
--- reactive-app/src/main/java/clickortime/MailEvent.java
+++ reactive-app/src/main/java/clickortime/MailEvent.java
@@ -1,4 +1,27 @@
 package clickortime;
 
 public class MailEvent {
+    private String message;
+    private String email;
+
+    public MailEvent(String message) {
+        this.message = message;
+    }
+
+    public MailEvent(String message, String email) {
+        this.message = message;
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "MailEvent{" +
+                "message='" + message + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
