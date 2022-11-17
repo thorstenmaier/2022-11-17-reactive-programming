20a5e4064d46849876ef03395e1d8882b4e116cd
Thorsten Maier <thorsten.a.maier@accenture.com>
2022-11-17 11:01:20 +0100

    Subscriber mit onNext, onError und onComplete

--- reactive-app/src/main/java/ProgrammaticalEvents.java
+++ reactive-app/src/main/java/ProgrammaticalEvents.java
@@ -1,11 +1,17 @@
+import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public class ProgrammaticalEvents {
     public static void main(String[] args) {
         PublishSubject<String> subject = PublishSubject.create();
-        subject.subscribe(System.out::println);
+        subject.subscribe(
+                s -> System.out.println(s),
+                e -> e.printStackTrace(),
+                () -> System.out.println("COMPLETE")
+        );
 
         subject.onNext("Hallo");
         subject.onNext("Welt");
+        subject.onComplete();
     }
 }
