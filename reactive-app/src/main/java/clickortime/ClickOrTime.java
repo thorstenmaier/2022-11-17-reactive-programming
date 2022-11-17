21bd1228544658b9b2bb768f70e80f40336d9939
Thorsten Maier <thorsten.a.maier@accenture.com>
2022-11-17 13:39:13 +0100

    Click or Time - Loesung 1

--- reactive-app/src/main/java/clickortime/ClickOrTime.java
+++ reactive-app/src/main/java/clickortime/ClickOrTime.java
@@ -5,11 +5,17 @@ import io.reactivex.rxjava3.core.Observable;
 import java.util.concurrent.TimeUnit;
 
 public class ClickOrTime {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Observable<TimeEvent> timer = Observable.interval(3, TimeUnit.SECONDS)
                 .map(i -> new TimeEvent());
 
         Observable<ClickEvent> clicks = Observable.interval(5, TimeUnit.SECONDS)
                 .map(i -> new ClickEvent());
+
+        Observable.merge(timer, clicks).subscribe(e -> {
+            System.out.println("SEND MAIL " + e);
+        });
+
+        Thread.sleep(10_000);
     }
 }
